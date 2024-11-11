44bfb20954e9cb0cb327803a2cfdacaae866b60f
Quirino <vquirino301@gmail.com>
2024-11-11 13:24:51 -0300

    add

---
+++ aula30/script.js
@@ -0,0 +1,8 @@
+//const n1 = 5;
+//const n2 = 5;
+//const result = n1 + n2;
+//console.log(result)
+const numer1  =  parseInt(prompt('dg 1 n:'));
+const numer2  =  parseInt(prompt('dg 1 n:'));
+const result = numer1 + numer2;
+alert('resultado da soma é : ' + result ) 
